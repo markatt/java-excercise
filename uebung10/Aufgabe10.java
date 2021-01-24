7d77694474decf9f384b32da1eb67934d2bf6cfd
Mark Attrot <cs.mark.attrot@gmail.com>
2021-01-24 13:32:56 +0100

    Add input for array Size & for Loop to fill Array

---
+++ uebung10/Aufgabe10.java
@@ -0,0 +1,30 @@
+package uebung10;
+
+import java.util.Random;
+
+public class Aufgabe10 {
+    public static void main(String[ ] args) {
+        int [ ] x = erzeugenUndAusfuellen ( );
+        // TODO ... 
+    }
+    public static int[ ] erzeugenUndAusfuellen ( ) {
+        Random randomNum = new Random();
+        java.util.Scanner eingabe = new java.util.Scanner ( System.in ); System.out.println("Bitte geben Sie die Anzahl der Zahlen: "); 
+        int anzahlVonZahlen = eingabe.nextInt();
+        int[ ] zahlen = new int[anzahlVonZahlen];
+        
+        for(int i = 0; i < zahlen.length; i++) {
+            zahlen[i] = randomNum.nextInt(50);
+        }
+        eingabe.close();
+        return zahlen; 
+    }
+    public static int maximumZahlFinden ( int[ ] zahlen ) { int max = 0;
+        // TODO ...
+        return max;
+    }
+    public static double durchschnittRechnen ( int[ ] zahlen ) {
+        double durchschnitt = 0; // TODO ...
+        return durchschnitt;
+    } 
+}
