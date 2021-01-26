0f97ad65041dce37b5ec235313276c2996e0f783
Mark Attrot <cs.mark.attrot@gmail.com>
2021-01-26 17:37:47 +0100

    Add for Loop in listeErstellen to fill multidimensional array with random nums (e.g. Studen-Id & Grade)

--- aufgabe11.java
+++ aufgabe11.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class Aufgabe11 {
     public static void main(String[] args) {
         listeErstellen();
@@ -17,6 +19,16 @@ class Aufgabe11 {
          * Add Random nums 100000 999999 to fill 1st column
          * Add Random grades from 1 to 6 to fill 2nd column
          */
+
+        for(int i = 0; i < notenListe.length ; i++) {
+            Random randNum = new Random();
+
+            notenListe[i][0] = randNum.nextInt((1000000 - 100000) + 1) - 100000;
+
+            notenListe[i][1] = randNum.nextInt(7);
+
+            
+        }
         return notenListe;
     }
 }
