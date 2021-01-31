2167eeca580361395062e9a3bb2655456fccf663
Mark Attrot <cs.mark.attrot@gmail.com>
2021-01-31 20:24:49 +0100

    Add Comments and bug fix

--- uebung11/aufgabe11.java
+++ uebung11/aufgabe11.java
@@ -12,7 +12,6 @@ class Aufgabe11 {
     
             } catch (InputMismatchException e) {
                 System.out.println("Bitte geben Sie nur ganze Zahlen an.");
-                System.out.println("Wollen Sie das Programm noch einmal starten? \n Geben Sie 0 für nein und 1 für ja ein.");
             }
     }
 
@@ -20,20 +19,20 @@ class Aufgabe11 {
         java.util.Scanner eingabe = new java.util.Scanner(System.in);
         System.out.println("Wie lang soll ihre Liste sein (Zeilen)?");
         int dimension = eingabe.nextInt();
-
+        //Create an multidimensional Array with 2 columns and n rows
         int[][] notenListe = new int[dimension][2];
 
         eingabe.close();
 
         /**
-         * @Bug still generates negatives Numbers
+         * @Bug still generates Numbers less than 6-digits
          */
 
         for(int i = 0; i < notenListe.length ; i++) {
             Random randNum = new Random();
-
+            //Generating 6-digit Student-Id
             notenListe[i][0] = randNum.nextInt((999999 - 100000) + 100000);
-
+            //Generating grade from 1 to 6
             notenListe[i][1] = randNum.nextInt(7) + 1;
          
         }
@@ -53,19 +52,21 @@ class Aufgabe11 {
     }
 
     public static void besteNoteAusgeben(int[][] liste){
-        int [][] bestGrade = new int [liste.length][liste.length];
+        //Initialize bestGrade Array with length of passed in list
+        int [][] bestGrade = new int [liste.length][2];
 
         bestGrade[0][0] = liste[0][0];
         bestGrade[0][1] = liste[0][1]; 
 
         /**
          * Check for best Grade
-         * If there are more best grades they get pushed into the array
+         *
          */
         for(int i = 1; i < liste.length; i++) {
             if(bestGrade[0][1] > liste[i][1]){
                 bestGrade[0][0] = liste[i][0];
                 bestGrade[0][1] = liste[i][1];
+            // If there are more best grades they get pushed into the array, too
             } else if(bestGrade[0][1] == liste[i][1]){
                 bestGrade[bestGrade.length - 1][0] = liste[i][0];
                 bestGrade[bestGrade.length - 1][1] = liste[i][1];
